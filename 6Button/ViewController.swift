04cbc9c36f9b14619b54c924aed1958eb8d52537
David Phillip Oster <DavidPhillipOster@gmail.com>
2023-01-30 21:14:48 -0800

    NSAppTransportSecurity and cachePolicy

--- 6Button/ViewController.swift
+++ 6Button/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
       "http://10.240.101.69/stateFull.xml?relay1state=2",
       "http://10.240.101.69/stateFull.xml?relay2state=2",
     ]
-    let task = URLSession.shared.dataTask(with: URL(string:command[sender.tag])!)
+    let request = URLRequest(url: URL(string:command[sender.tag])!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
+    let task = URLSession.shared.dataTask(with: request)
     task.resume()
   }
 
